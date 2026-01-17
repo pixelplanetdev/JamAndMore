2f363913d79a4dc73b8846560e3b0dd167b04a6f
prodcautious <125836095+prodcautious@users.noreply.github.com>
2026-01-16 22:55:28 -0500

    actual initial commit

---
+++ src/main/java/dev/pixelplanet/JamAndMore.java
@@ -0,0 +1,22 @@
+package dev.pixelplanet;
+
+import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import dev.pixelplanet.commands.ExampleCommand;
+import dev.pixelplanet.events.ExampleEvent;
+
+import javax.annotation.Nonnull;
+
+public class JamAndMore extends JavaPlugin {
+
+    public JamAndMore(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        this.getCommandRegistry().registerCommand(new ExampleCommand("example", "An example command"));
+        this.getEventRegistry().registerGlobal(PlayerReadyEvent.class, ExampleEvent::onPlayerReady);
+    }
+}
